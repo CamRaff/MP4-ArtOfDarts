4a4d48894cc6ab16a8e085b16b42fb41cb838283
Cameron Rafferty <cameron.rafferty@hotmail.co.uk>
2025-01-28 13:35:39 +0000

    Initial set up of Django Allauth

--- art_of_darts/settings.py
+++ art_of_darts/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites', # Used by socialaccount for URL callbacks
+    'allauth', # Allauth
+    'allauth.account', # Allows registrating, login/out, password resets
+    'allauth.socialaccount', # Allows social media logins
 ]
 
 MIDDLEWARE = [
@@ -59,13 +65,22 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                'django.template.context_processors.request', # Required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django Admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+SITE_ID = 1 # Used by socialaccount for URL callbacks
 
 WSGI_APPLICATION = 'art_of_darts.wsgi.application'
 
