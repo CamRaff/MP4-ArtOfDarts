1ca205211957b0580bdfea64e73f17935912bf62
Cameron Rafferty <cameron.rafferty@hotmail.co.uk>
2025-01-31 14:50:21 +0000

    added some products via admin and introduction of all products page

--- products/forms.py
+++ products/forms.py
@@ -18,14 +18,14 @@ class ProductForm(forms.ModelForm):
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = friendly_names
-        # self.fields['barrels'] = forms.ChoiceField(
-        #     choices=[(b.id, b.get_shape_display()) for b in Barrel.objects.all()]
-        # )
-        # self.fields['flights'] = forms.ChoiceField(
-        #     choices=[(f.id, f.get_shape_display()) for f in Flight.objects.all()]
-        # )
-        # self.fields['stems'] = forms.ChoiceField(
-        #     choices=[(s.id, s.get_length_display()) for s in Stem.objects.all()]
-        # )
+        self.fields['barrels'] = forms.ChoiceField(
+            choices=[(b.id, b.get_shape_display()) for b in Barrel.objects.all()]
+        )
+        self.fields['flights'] = forms.ChoiceField(
+            choices=[(f.id, f.get_shape_display()) for f in Flight.objects.all()]
+        )
+        self.fields['stems'] = forms.ChoiceField(
+            choices=[(s.id, s.get_length_display()) for s in Stem.objects.all()]
+        )
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded'
--- products/views.py
+++ products/views.py
@@ -4,8 +4,21 @@ from .models import Product, Category, Barrel, Flight, Stem
 from .forms import ProductForm
 
 
+def all_products(request):
+    """ A view to show all products, including sorting and search queries """
+
+    products = Product.objects.all()
+    template = 'products/all_products.html'
+    context = {
+        'products': products,
+    }
+
+    return render(request, template, context)
+
+
 def add_product(request):
     """ Add a product to the store """
+
     form = ProductForm()
     template = 'products/add_product.html'
     context = {
