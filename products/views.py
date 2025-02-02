334cc37374b44f8001c291e55d38b16a23e8380b
Cameron Rafferty <cameron.rafferty@hotmail.co.uk>
2025-02-02 14:04:27 +0000

    category and product type filtering

--- products/views.py
+++ products/views.py
@@ -12,8 +12,29 @@ def all_products(request):
     products = Product.objects.all()
     template = 'products/all_products.html'
     query = None
+    category = None
+    product_type = request.GET.get('product')
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if product_type:
+            if product_type in ['standard', 'kite']:  # Flight shapes
+                products = Flight.objects.filter(
+                    flight_shape__iexact=product_type
+                    )
+            elif product_type in ['tapered', 'straight']:  # Barrel shapes
+                products = Product.objects.filter(
+                    barrel__barrel_shape__iexact=product_type
+                    )
+            elif product_type in ['short', 'medium', 'long']:  # Stem lengths
+                products = Product.objects.filter(
+                    stem__stem_length__iexact=product_type
+                    )
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -29,6 +50,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': category,
     }
 
     return render(request, template, context)
