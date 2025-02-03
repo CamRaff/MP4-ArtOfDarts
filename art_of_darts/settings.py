1fb3d6673e004377639509dc63fbfbaec45dd9b3
Cameron Rafferty <cameron.rafferty@hotmail.co.uk>
2025-02-03 14:36:21 +0000

    added contexts.py to bag app and enabled it across all apps

--- art_of_darts/settings.py
+++ art_of_darts/settings.py
@@ -84,6 +84,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # Required
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',  # Bag contents
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
@@ -176,6 +177,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR / 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
