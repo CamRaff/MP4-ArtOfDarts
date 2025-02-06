570eed0612a184aac331d80ae333a0cafac4e49e
Cameron Rafferty <cameron.rafferty@hotmail.co.uk>
2025-02-06 13:21:18 +0000

    fix bug stopping you adding something to the bag that was already there

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import (
     render, redirect, get_object_or_404, reverse, HttpResponse
 )
+from django.contrib import messages
 from products.models import Product, Barrel, Stem, Flight
 
 
@@ -45,14 +46,19 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
+    item_id = str(item_id)
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
+        messages.success(
+            request, f'Successfully added {product.name} to your shopping bag'
+            )
 
     request.session['bag'] = bag
     return redirect(redirect_url)
