44e6e6137e51c4acf34e4046c9e9e1a8fa2d693f
Cameron Rafferty <cameron.rafferty@hotmail.co.uk>
2025-02-06 16:36:22 +0000

    updates to checkout admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderLineItem
+
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    """ Define the columns that will be displayed in the admin panel """
+
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    """ Define the columns that will be displayed in the admin panel """
+
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ('order_number', 'date', 'delivery_cost',
+                       'order_total', 'grand_total',)
+
+    fields = (
+        'order_number',
+        'date',
+        'full_name',
+        'email',
+        'phone_number',
+        'street_address1',
+        'street_address2',
+        'town_or_city',
+        'postcode',
+        'order_total',
+        'delivery_cost',
+        'grand_total',
+    )
+
+    list_display = (
+        'order_number',
+        'date',
+        'full_name',
+        'order_total',
+        'delivery_cost',
+        'grand_total',
+    )
+
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
