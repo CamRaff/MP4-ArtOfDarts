f0d64a594108e9ced093c266ea36ec91e3de7429
Cameron Rafferty <cameron.rafferty@hotmail.co.uk>
2025-02-10 16:06:53 +0000

    Stripe element in place

--- checkout/views.py
+++ checkout/views.py
@@ -17,6 +17,8 @@ def checkout(request):
 
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QqyRVLwvVySyyz5crr5DceVnyDDcyaDGGBZSr0jLiT9Zppl9g6cTd7kIGRoV49zOngnxSPXYq28XuXwqbPxHMWy00ELgbXG9h',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
