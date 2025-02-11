064c0444ca76f85f821d583acd66537847bd621d
Cameron Rafferty <cameron.rafferty@hotmail.co.uk>
2025-02-11 18:27:07 +0000

    webhooks initialized and tested

--- art_of_darts/settings.py
+++ art_of_darts/settings.py
@@ -34,7 +34,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-camraff-mp4artofdarts-yuhmx98ljrn.ws.codeinstitute-ide.net',
-    '127.0.0.1'
+    '127.0.0.1',
+    'localhost',
 ]
 
 
@@ -191,6 +192,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
