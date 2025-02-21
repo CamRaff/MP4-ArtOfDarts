9fc3d621b41d06b438ee267487aa77dcaf3495d2
Cameron Rafferty <cameron.rafferty@hotmail.co.uk>
2025-02-21 13:29:38 +0000

    further steps to deployment

--- art_of_darts/settings.py
+++ art_of_darts/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-camraff-mp4artofdarts-yuhmx98ljrn.ws.codeinstitute-ide.net',
