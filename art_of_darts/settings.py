035dc5d2669a2fcf3a42b0447991d3c9f69aecce
Cameron Rafferty <cameron.rafferty@hotmail.co.uk>
2025-02-24 12:27:30 +0000

    further attempts at fixing order confirmation emails

--- art_of_darts/settings.py
+++ art_of_darts/settings.py
@@ -200,8 +200,8 @@ if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'art-of-darts-mp4'
     AWS_S3_REGION_NAME = 'eu-north-1'
-    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and Media files
